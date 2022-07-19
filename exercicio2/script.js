787268c3f21ee1e08b6fce2b435141e1c314eb17
Dhieuri <dhieuri@gmail.com>
2022-07-19 02:06:14 -0300

    Atividades da aula007 sobre java script, variaveis e tipos primitivos

---
+++ exercicio2/script.js
@@ -0,0 +1,22 @@
+/*const torta = prompt("Você gosta de torta de Maçã?");
+const esport= prompt("Você curte esportes?");
+const estudo= prompt("Você sabe o que é Dev full-stack?");
+
+console.log(estudo);
+console.log(torta);
+console.log(esport);*/
+
+const tortaperg = prompt("Você gosta de torta de maça?");
+const tortaresp = prompt(tortaperg);
+console.log("Você gosta de torta de maça?", tortaperg);
+console.log(tortaresp);
+
+const sportperg = prompt("Você curte esportes?");
+const sportresp = prompt(sportperg);
+console.log("Você curte esportes?", sportperg);  
+console.log(sportresp);
+
+const didaticaperg = prompt("Você sabe o que é Dev full-stack?");
+const didaticaresp = prompt(didaticaperg);
+console.log("Você sabe o que é Dev full-stack?", didaticaperg);  
+console.log(didaticaresp);
