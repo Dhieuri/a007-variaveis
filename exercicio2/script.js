ac442e042c895aa8c7b73b60d46cc27f9bc0d80c
Dhieuri <dhieuri@gmail.com>
2022-07-19 21:24:25 -0300

    modificação do execicio 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -5,18 +5,18 @@ const estudo= prompt("Você sabe o que é Dev full-stack?");
 console.log(estudo);
 console.log(torta);
 console.log(esport);*/
+const tortaresp = "Não!";
+const sportresp = "Sim!";
+const estudoresp = "Sim!";
 
 const tortaperg = prompt("Você gosta de torta de maça?");
-const tortaresp = prompt(tortaperg);
-console.log("Você gosta de torta de maça?", tortaperg);
-console.log(tortaresp);
+console.log("Você gosta de torta de maça?", tortaresp);
+
 
 const sportperg = prompt("Você curte esportes?");
-const sportresp = prompt(sportperg);
-console.log("Você curte esportes?", sportperg);  
-console.log(sportresp);
+console.log("Você curte esportes?", sportresp);  
 
-const didaticaperg = prompt("Você sabe o que é Dev full-stack?");
-const didaticaresp = prompt(didaticaperg);
-console.log("Você sabe o que é Dev full-stack?", didaticaperg);  
-console.log(didaticaresp);
+
+const esstudoperg = prompt("Você sabe o que é Dev full-stack?");
+console.log("Você sabe o que é Dev full-stack?", estudoresp);  
+
